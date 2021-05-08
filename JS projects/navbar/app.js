d4446aacce38e75c9827ea286a32703fd20169d5
Fatemeh-Alizadeh <82482430+Fatemeh-Alizadeh@users.noreply.github.com>
2021-05-08 22:32:03 +1000

    js

--- JS projects/navbar/app.js
+++ JS projects/navbar/app.js
@@ -1,7 +1,10 @@
 const btn = document.querySelector(".btn");
 const navbar = document.querySelector(".navbar");
-console.log(navbar)
+const line = document.querySelector(".line");
+const logo = document.querySelector(".logo");
 
-btn.addEventListener("click", () => {
-    btn.classList.toggle('change');
+btn.addEventListener("click", function () {
+    navbar.classList.toggle('nav-menu')
+    logo.classList.toggle('logo-change')
+    
 });
